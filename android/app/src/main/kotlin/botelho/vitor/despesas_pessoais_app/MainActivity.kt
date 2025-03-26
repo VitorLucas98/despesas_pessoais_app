ec25200f3c109ccb474f66578532f81e443ff56f
Vítor Botelho <vitorluc.2013@gmail.com>
2025-03-26 08:45:31 -0300

    Criação do projeto

---
+++ android/app/src/main/kotlin/botelho/vitor/despesas_pessoais_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package botelho.vitor.despesas_pessoais_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Despesas Pessoais',
+          style: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color(0xFF6A1B9A), // Cor hexadecimal
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0), // Espaço para cima e para baixo
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white, // Fundo diferente
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.add, color: Colors.purple), // Cor do ícone
+                onPressed: () {
+                  // Add action here
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Versão Inicial'),
+      ),
+    );
+  }
+}
