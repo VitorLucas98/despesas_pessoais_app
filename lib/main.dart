de7461468a53167c4772fda13d90a8dc33e443cc
Vítor Botelho <vitorluc.2013@gmail.com>
2025-03-26 21:51:46 -0300

    Adicionando modelo transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas_pessoais_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -7,14 +8,27 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-      );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +46,9 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
         actions: [
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0), // Espaço para cima e para baixo
+            padding: const EdgeInsets.symmetric(
+              vertical: 8.0,
+            ), // Espaço para cima e para baixo
             child: Container(
               decoration: BoxDecoration(
                 color: Colors.white, // Fundo diferente
@@ -48,8 +64,18 @@ class MyHomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Versão Inicial'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(color: Colors.blue, elevation: 5, child: const Text('Gráfico')),
+          Column(
+            children:
+                _transactions.map((tr) {
+                  return Card(child: Text(tr.title));
+                }).toList(),
+          ),
+        ],
       ),
     );
   }
