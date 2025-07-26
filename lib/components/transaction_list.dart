1af5e94bcefb4529964ab788753609de0fb768f5
Vitor Lucas <vitorluc.2013@gmail.com>
2025-07-26 13:46:33 -0300

    adicionando imagem

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -15,51 +15,71 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.purple, width: 2),
+      child:
+          transactions.isEmpty
+              ? Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    'Nenhuma transação cadastrada!',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    _formatCurrency(tr.value),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
+                  const SizedBox(height: 20),
+                  Container(
+                    height: 200,
+                    child: Image.asset('assets/images/waiting.png',
+                     fit: BoxFit.cover),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                ],
+              )
+              : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (ctx, index) {
+                  final tr = transactions[index];
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            _formatCurrency(tr.value),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tr.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              DateFormat(
+                                'dd/MM/yyyy HH:mm',
+                              ).format(tr.date.subtract(Duration(hours: 3))),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Text(
-                      DateFormat(
-                        'dd/MM/yyyy HH:mm',
-                      ).format(tr.date.subtract(Duration(hours: 3))),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+                  );
+                },
+              ),
     );
   }
 }
